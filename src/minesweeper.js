275432297ea15ed615e5d8c728ca1770a04a9d66
Zachary Thomas <zacharythomas@Jakes-MacBook-Pro-3.local>
2018-02-26 21:30:49 -0800

    2nd lesson

---
+++ src/minesweeper.js
@@ -0,0 +1,18 @@
+const printBoard = (board) => {
+  console.log('Current Board:')
+  console.log(board[0].join('|'));
+  console.log(board[1].join('|'));
+  console.log(board[2].join('|'));
+}
+
+let board = [
+    [' ',' ',' '],
+    [' ',' ',' '],
+    [' ',' ',' ']
+];
+
+console.log(printBoard(board));
+
+board[0][1] = '1'
+board[2][2] = 'B'
+console.log(printBoard(board));
