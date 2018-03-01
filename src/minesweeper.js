60ae9d82df83e5c53f8a6e6fb7e2751850085755
Zachary Thomas <zacharythomas@Jakes-MacBook-Pro-3.local>
2018-02-28 23:03:12 -0800

    Day something updates

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,18 +1,69 @@
-const printBoard = (board) => {
-  console.log('Current Board:')
-  console.log(board[0].join('|'));
-  console.log(board[1].join('|'));
-  console.log(board[2].join('|'));
+// two boards: one for player's guesses and the other
+// for bomb locations
+
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+  // don't forget .push()
+  board = []
+  for (i=0; i < numberOfRows; i++){
+    row = []
+      for (j=0; j < numberOfColumns; j++){
+        row.push('  ')
+      }
+    board.push(row)
+  }
+  return board
+};
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
 }
 
-let board = [
-    [' ',' ',' '],
-    [' ',' ',' '],
-    [' ',' ',' ']
-];
+const placeBombs = (board, numberOfBombs) => {
+  // assumes a blank board
+  let rows = board.length;
+  let columns = board[0].length;
 
-console.log(printBoard(board));
+  // check if number of bombs plausible
+  if (numberOfBombs > rows*columns){
+    return 'ERROR: more bombs than space on board'
+  }
 
-board[0][1] = '1'
-board[2][2] = 'B'
-console.log(printBoard(board));
+  // insert each bomb into a spot on board not already occupied by a bomb
+  let bombCounter =0
+  while (bombCounter < numberOfBombs){
+    let guessRow = getRandomInt(rows);
+    let guessColumn = getRandomInt(columns);
+    if (board[guessRow][guessColumn] == 'B '){
+      continue;
+    }
+    else {
+      board[guessRow][guessColumn] = 'B '
+      bombCounter++;
+    };
+  }
+  return board
+}
+
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+  let board = generatePlayerBoard(numberOfRows, numberOfColumns);
+  let bombedBoard = placeBombs(board, numberOfBombs);
+  return bombedBoard;
+}
+
+// Function to print off board
+const printBoard = (board) => {
+  for (i=0; i<board.length; i++){
+    console.log(board[i].join('|'));
+  }
+}
+
+
+// Mimic Codecademy functionality
+const codeCademyPrint = (numberOfRows, numberOfColumns, numberOfBombs) => {
+  console.log('Player Board:');
+  printBoard(generatePlayerBoard(numberOfRows,numberOfColumns));
+  console.log('Bomb Board:');
+  printBoard(generateBombBoard(numberOfRows,numberOfColumns,numberOfBombs));
+}
+
+codeCademyPrint(3,3,5);
---
+++ src/test.js
@@ -0,0 +1,3 @@
+let board = [['', '']]
+board[0][1] = 'g'
+console.log(board)
