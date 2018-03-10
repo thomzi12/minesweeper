45845f50a34bf43361f48ab63800e2ae70c38de0
Zachary Thomas <zacharythomas@Jakes-MacBook-Pro-3.local>
2018-03-10 13:03:12 -0800

    Added Player Interactivity

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -44,10 +44,79 @@ const placeBombs = (board, numberOfBombs) => {
   return board
 }
 
+
 const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
   let board = generatePlayerBoard(numberOfRows, numberOfColumns);
   let bombedBoard = placeBombs(board, numberOfBombs);
   return bombedBoard;
+}
+
+// Helper function to get the coordinates of the sister cells as an array
+const _getOffsetTiles = (bombedBoard, rowIndex, columnIndex) => {
+  boardRows = bombedBoard.length;
+  boardColumns = bombedBoard[0].length;
+
+  // Check that inputted row and index are realistic
+  if (rowIndex < 0 || rowIndex>boardRows || columnIndex < 0
+      || columnIndex > boardColumns){
+    return 'invalid row or column index'
+  }
+
+  // Create and return array with locations of neighbor indices
+  return_array = []
+
+  for (let i = -1; i<=1; i++){
+    for (let j = -1; j<=1; j++){
+      // Check eligble spaces around user choice, exclude that point itself
+      if (((rowIndex + i) < boardRows) && ((rowIndex + i) >= 0)
+        && ((columnIndex + j) < boardColumns) && ((columnIndex + j) >= 0)
+        && !(i === 0 && j === 0)){
+           return_array.push([rowIndex + i, columnIndex + j])
+          }
+    }
+  }
+  return return_array;
+}
+
+// Function to display the number of bombs adjacent to the flipped tile
+const getNumberOfNeighborBombs = (bombedBoard, userBombGuess) => {
+  if (!Array.isArray(userBombGuess)){
+    return "User guess should be an array"
+  }
+
+  let offsetTiles = _getOffsetTiles(bombedBoard, userBombGuess[0], userBombGuess[1]);
+
+  numberBombs = 0
+  for (i = 0; i< offsetTiles.length; i++){
+    if (bombedBoard[offsetTiles[i][0]][offsetTiles[i][1]] == 'B '){
+      numberBombs++;
+    }
+  }
+
+  return numberBombs;
+}
+
+// Function for user to flip a tile
+// Note: bombBoard is meant to be static. Update guesses to the player board
+const flipTile = (playerBoard, bombBoard, rowIndex, columnIndex) => {
+
+  // Has tile already been flipped by player?
+  if (playerBoard[rowIndex][columnIndex] != '  '){
+    console.log('Tile has already been flipped!');
+  }
+
+  // Is there a bomb? If so, place on player board
+  else if (bombBoard[rowIndex][columnIndex].includes('B')){
+    playerBoard[rowIndex][columnIndex] = 'B ';
+  }
+
+  // No bomb? Then how many neighboring bombs?
+  else {
+    playerBoard[rowIndex][columnIndex] = getNumberOfNeighborBombs(bombBoard,
+                                          [rowIndex, columnIndex]).toString()
+                                          + ' ';
+  }
+  return playerBoard;
 }
 
 // Function to print off board
@@ -66,4 +135,19 @@ const codeCademyPrint = (numberOfRows, numberOfColumns, numberOfBombs) => {
   printBoard(generateBombBoard(numberOfRows,numberOfColumns,numberOfBombs));
 }
 
-codeCademyPrint(3,3,5);
+// codeCademyPrint(3,3,5);
+
+// Mimic Codecademy functionality
+const codeCademyFlipTilePrint = (numberOfRows, numberOfColumns, numberOfBombs,
+                                guessRow, guessColumn) => {
+  let playerBoard = generatePlayerBoard(3,3);
+  let bombBoard = generateBombBoard(3,3,3);
+  console.log('Player Board:');
+  printBoard(playerBoard);
+  console.log('Bomb Board');
+  printBoard(bombBoard);
+  console.log('Updated Player Board:');
+  printBoard(flipTile(playerBoard, bombBoard, guessRow,guessColumn));
+}
+
+codeCademyFlipTilePrint(3,3,3,0,0);
